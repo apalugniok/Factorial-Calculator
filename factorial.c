b207687929bbdc0f41e138e9cbb505ba0c8ab613
Andrew Palugniok <apalugniok@icloud.com>
2017-10-29 12:31:06 +0000

    Add handling of corner cases for user input, Tidy up code.

--- factorial.c
+++ factorial.c
@@ -2,108 +2,168 @@
 #include "stdlib.h"
 #include "string.h"
 #include "math.h"
+#include "ctype.h"
 
-int result_size; //Global as it's needed to be editable in many functions
+int result_size;
 int number_size;
 
-int* ArrayConvert() // Convert number into reverse order array of its digits
-{
-	char number[400];
-	scanf("%s", number); //input number to find the factorial for
-	number_size = strlen(number);
-	int* ptr = calloc(number_size, sizeof(int)); //covert char string entries to int and store in *ptr
-	for (int i = (number_size - 1); i > -1; i--)
-	{
-		ptr[number_size - 1 - i] = (int)(number[i] - '0'); 
-	}
-	return ptr;
-}
-	
-int* NumberSubtract(int* num)//Subracts 1 from array number
-{
-	if (num[number_size -1] == 0)//Check is num is non zero works on premise that num will have no leading 0s
-	{
-		return num; //If it is dont do anything
-	}
-	else //Otherwise subract one from last digit
-	{
-		num[0]--;
-		for (int i = 0; i < number_size - 1; i++) 
-		{
-			if (num[i] < 0) //check if the subtraction makes the value negative if yes make the value 9 and take 1 from next cell
-			{
-				num[i] = 9;
-				num[i+1]--;
-			}
-			else // if the the necessary subtractions didnt make any entry <0 exit out of the function
-			{
-				return num;
-			}
-		}
-		if (num[number_size - 1] == 0) //check if subtractions left a leading 0 if yes reallocate memory to get rid of it
-			number_size--;
-			num = realloc(num, (number_size * sizeof(int)));
-			return num;
+/* Take user input and process depending on cases below
 
-	}
+Case 0: Input is a positive number with no leading 0s
+  Convert input into reverse order array of its digits.
+Case 1: Input is positive but has leading 0s.
+  Convert input into reverse order array of its digits.
+  Once input is converted into the array number leading 0s are stripped off.
+Case 2: Input is 0 or many 0s.
+  Convert input into reverse order array of its digits.
+  All 0s will then be stripped off leaving an empty output with number_size = 0.
+Case 3: Input is a negative number.
+  Each char of the input is checked with isdigit().
+  "-" will not pass isdigit().
+  Set number_size to -1 to handle output in main().
+Case 4: Input contains none digit numbers.
+  Acts the same as Case 3. */
+int* array_convert()
+{
+  char number[400];
+  scanf("%s", number);
+  number_size = strlen(number);
+  int* ptr = calloc(number_size, sizeof(int));
+  for (int i = (number_size - 1); i > -1; i--)
+  {
+    if (isdigit(number[i]) > 0)
+    {
+      ptr[number_size - 1 - i] = (int)(number[i] - '0');
+    }
+    else
+    {
+      printf("ERROR: Input is not a positive integer or 0\n");
+      number_size = -1;
+      return ptr;
+    }
+  }
+
+  // Get rid of leading 0s in input
+  for (int i = (number_size - 1); i > -1 ; i--)
+  {
+    if (ptr[i] == 0)
+    {
+      (number_size)--;
+    }
+    else
+    {
+      break;
+    }
+  }
+  ptr = realloc(ptr, (number_size * sizeof(int)));
+
+  return ptr;
 }
 
-int* NumberMultiply(int* num1,int digits_num1, int* num2, int digits_num2) //Multiplies two array numbers using column multiplication method
+// Subracts 1 from array number using long subtraction method
+int* number_subtract(int* num)
 {
-	result_size = digits_num1 + digits_num2; //determine maximum size of result
-	int* result = (int*)calloc(result_size, sizeof(int));
-	for (int i = 0; i < digits_num2; i++) //Column multiplication loops
-	{
-		for (int j = 0; j < digits_num1; j++)
-		{
-			result[i+j] += num2[i] * num1[j];
-			int mod = floorf(result[i+j] / 10); //Check whether to carry over and do so if needed
-			result[i+j+1] += mod;
-			result[i+j] -= (10*mod);
-			
-		}
-	}
-	for (int i = (result_size - 1); i > -1 ; i--) //Find out how many leading 0s
-	{
-		if (result[i] == 0)
-		{
-			(result_size)--;
-		}
-		else
-		{
-			break;
-		}
-	}
-	result = realloc(result, (result_size * sizeof(int))); //Reallocate memory to get rid of leading 0s
-	free(num2);
-	return result;
+  num[0]--;
+  for (int i = 0; i < number_size - 1; i++)
+  {
+    if (num[i] < 0)
+    {
+      num[i] = 9;
+      num[i+1]--;
+    }
+    else
+    {
+      return num;
+    }
+  }
+
+  //check if subtractions left a leading 0 and get rid of it
+  if (num[number_size - 1] == 0)
+  {
+    number_size--;
+    num = realloc(num, (number_size * sizeof(int)));
+  }
+  return num;
+}
+
+// Multiplies two array numbers using column multiplication method
+int* number_multiply(int* num1,int digits_num1, int* num2, int digits_num2)
+{
+  // Determine maximum size of result
+  result_size = digits_num1 + digits_num2;
+  int* result = (int*)calloc(result_size, sizeof(int));
+
+  for (int i = 0; i < digits_num2; i++)
+  {
+    for (int j = 0; j < digits_num1; j++)
+    {
+      result[i+j] += num2[i] * num1[j];
+      // Check whether to carry over and do so if needed
+      int mod = floorf(result[i+j] / 10);
+      result[i+j+1] += mod;
+      result[i+j] -= 10 * mod;
+    }
+  }
+
+  //Get rid of leading 0s
+  for (int i = (result_size - 1); i > -1 ; i--)
+  {
+    if (result[i] == 0)
+    {
+      (result_size)--;
+    }
+    else
+    {
+      break;
+    }
+  }
+  result = realloc(result, (result_size * sizeof(int)));
+  free(num2);
+  return result;
 }
 
 int main()
 {
-	printf("Enter a number to factorial: "); //Input number to find the factorial for
+  // Take in user input and convert to array number_ptr
+  printf("Enter a number to factorial: ");
+  int* number_ptr = array_convert();
+  int* result_ptr;
 
-	int* number_ptr = ArrayConvert();
-	int* result = malloc(number_size*sizeof(int));
-	result_size = number_size;
-	memcpy(result, number_ptr, number_size * sizeof(int)); //Store input number in n *result
-	number_ptr = NumberSubtract(number_ptr); //Make *number_ptr store n-1
+  // Check what case the input satified
+  if (number_size == -1)
+  {
+    // Input was not an allowed number
+    return 0;
+  }
+  else if (number_size == 0)
+  {
+    // Input was 0
+    result_ptr = malloc(sizeof(int));
+    result_size = 1;
+    result_ptr[0] = 1;
+  }
+  else
+  {
+    /* Input was an allowed number.
+    Copy number into result then take away 1 from number and begin loop which
+    sucessively multiplies result by number and takes 1 away from number until
+    number is 0 to calculate the factorial. */
+    result_ptr = malloc(number_size * sizeof(int));
+    result_size = number_size;
+    memcpy(result_ptr, number_ptr, number_size * sizeof(int));
+    number_ptr = number_subtract(number_ptr);
+    while (number_size > 0)
+    {
+      result_ptr = number_multiply(number_ptr, number_size, result_ptr, result_size);
+      number_ptr = number_subtract(number_ptr);
+    }
+  }
 
-	while (number_size>0) //Carry out factorial calculation
-	{
-		result = NumberMultiply(number_ptr, number_size, result, result_size);
-		number_ptr = NumberSubtract(number_ptr);
-	}
-
-	for (int i = (result_size - 1); i > -1; i--)
-	{
-		printf("%d", result[i]);
-	}
-
-	printf("\nDigits = %d\n", result_size);
-	free(number_ptr);
+  //Print resulting factorial
+  for (int i = (result_size - 1); i > -1; i--)
+  {
+    printf("%d", result_ptr[i]);
+  }
+  printf("\nDigits = %d\n", result_size);
+  free(number_ptr);
 }
-
-
-
-
